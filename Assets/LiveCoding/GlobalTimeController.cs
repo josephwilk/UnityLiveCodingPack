53e9df33a44c95043bf640ccbadaa7348526d7db
Joseph Wilk <joe@josephwilk.net>
2018-05-04 09:59:57 +0100

    Import

---
+++ Assets/LiveCoding/GlobalTimeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LiveCoding{
+    [AddComponentMenu("LiveCoding/Global Time Controller")]
+    public class GlobalTimeController : MonoBehaviour {
+        private float speedRatio = 1.0f;
+
+        public float _speedRatio {
+            get { return speedRatio; }
+            set { speedRatio = value; }
+        }
+
+        void Start () {}
+
+        void Update () {
+            Time.timeScale      = speedRatio;
+            Time.fixedDeltaTime = speedRatio*0.02f;
+        }
+    }
+}
