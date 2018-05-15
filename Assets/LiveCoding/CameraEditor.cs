2b9bc1dd50c78c9fcda6a46e028e96e5e0460ee9
Joseph Wilk <joe@josephwilk.net>
2018-05-15 11:00:53 +0100

    Useful fn for changing cameras

---
+++ Assets/LiveCoding/CameraEditor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LiveCoding{
+    [AddComponentMenu("LiveCoding/Camera Editor")]
+    public class CameraEditor : MonoBehaviour {
+
+        Camera[] cameras;
+        Camera liveCamera;
+
+	    void Start () {
+            cameras = Camera.allCameras;
+            liveCamera = Camera.main;
+
+            for (int i = 0; i < cameras.Length; i++) {
+                Camera cam = cameras [i];
+                cam.targetDisplay = 0;
+                cam.enabled = false;
+            }
+            liveCamera.enabled = true;
+	    }
+	
+        public void CameraChange(int newCamId){
+            if (newCamId < cameras.Length && newCamId >= 0) {
+                Camera newCam = cameras [newCamId];
+                if (newCam.enabled == false) {
+                    newCam.enabled = true;
+                    liveCamera.enabled = false;
+                    liveCamera = newCam; 
+                }
+            }
+        }
+
+	    void Update () {}
+    }
+}
